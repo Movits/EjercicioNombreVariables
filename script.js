2c7540c121b41c4fa6cd18a62315ce4880d0687e
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-18 00:13:01 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Analizar la seguiente informacion y definir el nombre de una variable ademas de un posible valor para la misma correspondiente al contexto de la informacion //
+
+let paisInit = "b";
+let msgReunion = "hola";
+let pesoBotellaAguaL = "2";
+let servTransp = "activo";
+let nombreCfp = "Centro de Formación Profesional N° 27 (sede) - Sindicato de Luz y Fuerza";
+let ytContrasena = "123";
+let nCel = "123456789";
+let postre = "brigadeiro";
