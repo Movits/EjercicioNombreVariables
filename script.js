d26868af7f9ea5f61670d3667ae60573c5a50ada
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-18 00:29:17 -0300

    Agrege prompts

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Analizar la seguiente informacion y definir el nombre de una variable ademas de un posible valor para la misma correspondiente al contexto de la informacion //
 
-let paisInit = "b";
-let msgReunion = "hola";
-let pesoBotellaAguaL = "2";
-let servTransp = "activo";
-let nombreCfp = "Centro de Formación Profesional N° 27 (sede) - Sindicato de Luz y Fuerza";
-let ytContrasena = "123";
-let nCel = "123456789";
-let postre = "brigadeiro";
+let paisInit = prompt ("Ingresar inicial de un pais");
+let msgReunion = prompt ("Escriba una mensaje de chat para una reunion");
+let pesoBotellaAguaL = prompt ("Ingresar el peso de una botella de agua en litros");
+let servTransp = true;
+let nombreCfp = prompt ("Ingrese el nombre del centro");
+let ytContrasena = prompt ("Ingrese su contrasena de youtube");
+let nCel = prompt ("Ingrese su numero de celular");
+let postre = prompt ("Ingrese un nombre de algun postre");
+let keyProducto = prompt ("Ingrese una key de algun producto comprado online");
